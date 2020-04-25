f6b7e3ba5bc4ddcbdf5b846f2cc6c1c3f6ba747a
Jake Froehlich <jakefroehlich@Jakes-MacBook-Pro.local>
2020-04-25 09:23:39 -0400

    stretch solution isEqual

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -1,5 +1,14 @@
 // This stretch requires you to write your own test cases
 
-const multiply = (num1, num2) => {};
+const multiply = (num1, num2) => {
+    if (!(num1 && num2)) {
+        throw 'failure';
+    }
+    else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        throw 'failure';
+    }
+    
+    return num1 * num2
+};
 
 module.exports = { multiply };
