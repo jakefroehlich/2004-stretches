108a4910d588a54a8ab518654a654b7329e8b4e3
Jake Froehlich <jakefroehlich@Jakes-MacBook-Pro.local>
2020-04-29 18:49:10 -0400

    completed exercise

--- questions/03_anagrams/anagrams.js
+++ questions/03_anagrams/anagrams.js
@@ -2,7 +2,18 @@
 // Return a boolean
 
 const checkAnagrams = (str1, str2) => {
-  //YOUR CODE
+  if (str1.length != str2.length) {
+    return false
+  }
+
+  for (let i = 0; i < str1.length; i++) {
+    let str1Letter = str1[i];
+
+    if (!str2.includes(str1Letter)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = { checkAnagrams };
