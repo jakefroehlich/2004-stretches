bfa3e894cddad5edb9af8dea9a0c916edf26d556
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-05-11 18:54:02 -0400

    completed exercise

--- questions/12_fizzBuzz/fizzBuzz.js
+++ questions/12_fizzBuzz/fizzBuzz.js
@@ -1,4 +1,25 @@
-const fizzBuzz = () => {
-  //YOUR CODE HERE
-};
+const fizzBuzz = (num) => {
+  if (typeof num !== 'number' || num < 0 || !(Number.isInteger(num))) {
+    throw Error;
+  }
+
+  let array = [];
+
+  for (let i = 0; i < num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      array.push('FizzBuzz');
+    }
+    else if (i % 5 === 0) {
+      array.push('Buzz');
+    }
+    else if (i % 3 === 0) {
+      array.push('Fizz');
+    }
+    else {
+      array.push(num)
+    }
+  };
+
+  return array;
+}
 module.exports = { fizzBuzz };
