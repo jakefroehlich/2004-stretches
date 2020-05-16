815df738f662e76b4225b6c91bfba87ec50d95f0
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-05-16 11:12:57 -0400

    completed

---
+++ questions/17_bookReview/bookReview.js
@@ -0,0 +1,27 @@
+class BookReview {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+        this.notes = {};
+        this.rating = 0;
+    }
+
+    addNotes(pageNum, note) {
+        if (!(this.notes[pageNum])){
+            this.notes[pageNum] = note;
+        }
+        else {
+            this.notes[pageNum] += ` ${note}`;
+        }
+    }
+
+    addRating(rating) {
+        if (rating > 5) {
+            throw Error;
+        }
+        else {
+            this.rating = rating;
+        }
+    }
+}
+module.exports = { BookReview };
