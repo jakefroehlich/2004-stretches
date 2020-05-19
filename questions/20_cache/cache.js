7ef7b505f98a8a8270a6b3f496305cbe2dfd13c8
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-05-19 18:06:45 -0400

    completed stretch

--- questions/20_cache/cache.js
+++ questions/20_cache/cache.js
@@ -1,7 +1,31 @@
 // see test specs
 
-function cache() {
-  // YOUR CODE
+function cache(func) {
+  if (typeof func != 'function') {
+    throw new Error('Input must be a function.');
+  };
+
+  let cache = {};
+
+  return function (arg) {
+    let args = [...arguments];
+    if (arguments.length > 1 && !cache[args]) {
+      cache[args] = func(...arguments);
+      return cache[args];
+    }
+    else if (arguments.length > 1) {
+      return cache[args];
+    };
+
+    if (!cache[arg]) {
+      cache[arg] = func(arg);
+      return cache[arg];
+    }
+    else {
+      return cache[arg];
+    };
+  };
 }
+
 
 module.exports = { cache };
