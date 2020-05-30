a60dc8d4bc7da24fac80f6b12f1da5a05dae0a44
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-05-30 09:20:31 -0400

    i think i was close

--- questions/27_cartesian/cartesian.js
+++ questions/27_cartesian/cartesian.js
@@ -1,5 +1,35 @@
-const cartesian = () => {
-  //YOUR CODE GOES HERE
+const opposites = {
+  n: 's',
+  s: 'n',
+  e: 'w',
+  w: 'e',
+}
+
+const finalObj = {
+  n: 0,
+  s: 0,
+  e: 0,
+  w: 0,
+}
+
+const cartesian = (array) => {
+  if (!Array.isArray(array)) {
+    throw new Error('Parameter must be an array.')
+  }
+
+  for (let i = 0; i < array.length; i + 2) {
+    let currentDir = array[i];
+    let nextDir = array[i + 1];
+
+    if (currentDir && nextDir) {
+      if (opposites[currentDir] !== nextDir) {
+        finalObj[currentDir]++
+      }
+    }
+  }
+
+  return finalObj;
+
 };
 
 module.exports = { cartesian };
--- questions/29_btownbakery/btownbakery.js
+++ questions/29_btownbakery/btownbakery.js
@@ -1,7 +1,27 @@
 // B-Town bakery over-bought the supplies to make cake and needs to know how many they can make all together.
 
-const btownbake = () => {
-  //enter code here
+const btownbake = (recipe, supplies) => {
+  if (typeof recipe !== 'object' || typeof supplies !== 'object') {
+    throw new Error ('Arguments must be objects!');
+  }
+
+  let maxNum = [];
+
+  for (let key in recipe) {
+    for (let supply in supplies) {
+      if (key === supply) {
+        let makeNum = supplies[supply] / recipe[key];
+
+        if (makeNum < 1) {
+          return 0;
+        }
+
+        maxNum.push(makeNum);
+      }
+    }
+  }
+
+  console.log(maxNum);
 };
 
 module.exports = { btownbake };
