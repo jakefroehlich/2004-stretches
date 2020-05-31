f29e9ed7a2a1dcb4f16ce73df5e34669ebab52b6
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-05-31 16:03:53 -0400

    completed today's exercise

--- questions/30_findBy/findBy.js
+++ questions/30_findBy/findBy.js
@@ -1,5 +1,10 @@
-const findBy = () => {
-  //YOUR CODE HERE
+const findBy = (array, key, value) => {
+
+  let found = array.find(obj => {
+    return obj[key] === value;
+  })
+  
+  return found;
 };
 
 module.exports = { findBy };
