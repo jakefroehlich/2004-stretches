4dc56b110f72cf25279cefd899363d7d691c6cb4
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-06-04 09:42:59 -0400

    completed exercise

--- questions/34_digitalRoot/digitalRoot.js
+++ questions/34_digitalRoot/digitalRoot.js
@@ -5,6 +5,25 @@ ex.
 55555 --> 5 + 5 + 5 + 5 + 5 = 25 --> 2 + 5 --> 7 
 */
 
-//YOUR CODE GOES HERE
+class digitalRoot {
+
+    root(num) {
+        if (num < 9 ) {
+            return num;
+        }
+        else {
+            let stringNum = num.toString();
+            let sum = 0;
+            
+            for (let i = 0; i < stringNum.length; ++i) {
+                let currentNum = stringNum[i];
+
+                sum += parseInt(currentNum)
+            }
+            
+            return this.root(sum);
+        }
+    }
+}
 
 module.exports = { digitalRoot };
