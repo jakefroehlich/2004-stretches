c2d1c333cca4c5e0de74366458cb21d000054107
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-06-08 18:55:56 -0400

    rough because I didn't do yesterdays lol

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -1,4 +1,39 @@
-const decypher = () => {
-  //YOUR CODE HERE
+const decypher = (string) => {
+
+  let code = '';
+  let thirdChar;
+
+  let rest;
+
+  for (let i = 0;i < string.length; i++) {
+    let char = string[i];
+
+    if (typeof char !== 'number') {
+      thirdChar = char;
+      rest = string.slice(i, string.length -1)
+      break;
+    }
+    else {
+      code += char;
+    }
+  }
+
+  console.log(code)
+
+  let firstChar = String.fromCharCode(parseInt(code));
+
+  let secondChar = string[string.length -1]
+
+
+  // for (let i = 0;i < string.length; i++) {
+  //   let char = string[i];
+
+  //   if (typeof char !== 'number') {
+  //     thirdChar = char;
+  //     rest = string.slice(i, string.length -1)
+  //     break;
+  //   }
+  // }
+  return `${firstChar}${secondChar}${thirdChar}${rest}`;
 };
 module.exports = { decypher };
