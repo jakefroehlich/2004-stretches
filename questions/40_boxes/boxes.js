c823271a2917151768939f900e0c553bc9b29a86
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-06-13 09:21:36 -0400

    did me best

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -1,3 +1,43 @@
 //YOUR CODE HERE
 
+class Box {
+    constructor(color, capacity) {
+        this.color = color;
+        this.capacity = capacity;
+        this.contents = [];
+    }
+
+    pack(color, cap) {
+
+        if( this.contents.length + 1 > this.capacity) {
+            throw new Error;
+        }
+
+            let box = new Box(color, cap);
+
+            this.contents.push(box);
+        
+    }
+
+    unpack() {
+        let colors = [];
+
+        for (let i = 0 ; i < this.contents.length; i++) {
+            let box = this.contents[i];
+
+            if(box.contents.length == 0) {
+                colors.push(box.color);
+            }
+            else {
+                colors.push(box.color);
+                for (let i = 0; i < box.contents.length; ++i) {
+                    colors.push(...box.unpack())
+                }
+            }
+        }
+
+        return colors;
+    }
+}
+
 module.exports = { Box };
